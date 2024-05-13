5202debc523bb95fc923cefc12584d37efd0af19
Mauro Rivadeneira <mauro3g@hotmail.com>
2024-05-12 22:36:43 -0500

    fix service

--- frontend/EthCert/src/app/services/certificates-service.service.ts
+++ frontend/EthCert/src/app/services/certificates-service.service.ts
@@ -12,7 +12,7 @@ import {
   providedIn: 'root',
 })
 export class CertificatesServiceService {
-  //private readonly API_URL = 'http://localhost:3000/';
+  //private readonly API_URL = 'http://localhost:8000/';
   private readonly API_URL = 'https://d8cfe026-bdbe-4cff-9d2b-e0402a169485.mock.pstmn.io/';
 
   constructor(private readonly httpClient: HttpClient) {}
@@ -29,7 +29,7 @@ export class CertificatesServiceService {
     return this.httpClient.get<IStudent[]>(this.API_URL + 'student-list');
   }
 
-  create_course(body: ICourse[]): Observable<string> {
+  create_course(body: ICourse): Observable<string> {
     return this.httpClient.post<string>(this.API_URL + 'course', body);
   }
 
@@ -38,6 +38,9 @@ export class CertificatesServiceService {
   }
 
   create_certificate(body: ICertificate): Observable<string> {
+    body.idcertificate = 0;
+    body.link = '';
+    body.hash = '';
     return this.httpClient.post<string>(this.API_URL + 'certificate', body);
   }
 
