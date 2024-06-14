8a1dcd45141d775782921bdd624df2b65361cde7
Mauro Rivadeneira <mauro3g@hotmail.com>
2024-06-14 15:41:20 -0500

    date format string validation

--- frontend/EthCert/src/app/utils/dateFormatter.ts
+++ frontend/EthCert/src/app/utils/dateFormatter.ts
@@ -1,5 +1,6 @@
 export class DateFormatterUtil {
-  formatDate(numberDate: number) {
+  formatDate(numberDate: number | string) {
+    if(typeof numberDate === 'string') return numberDate
     let formDate = new Date(numberDate);
     return formDate.toLocaleDateString();
   }
